f7da48d70d7f826a9a7f13d48037c5a2fd4f4eca
Willy Tan <willy.tan434@gmail.com>
2019-08-08 22:59:31 +0700

    Add dropdown view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import SC from "styled-components";
+import { FaBars } from "react-icons/fa";
+import colors from "../../constants/colors";
 
 const HeaderContainer = SC.div`
-  background-color: red;
+  background-color: ${colors.primaryColor};
+  border-bottom: 1px solid ${colors.primaryBorderColor};
+  display: flex;
+  flex: 1 1 auto;
+  padding: 8px 16px;
+`;
+
+const TitleBar = SC.div`
+  align-items: center;
+  color: ${colors.whiteColor};
+  display: flex;
+  flex: 1 1 auto;
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 2.25px;
+`;
+
+const HamburgerButton = SC.div`
+  align-items: center;
+  border: 1px solid ${colors.whiteColor};
+  border-radius: 4px;
+  color: ${colors.whiteColor};
+  cursor: pointer;
+  display: flex;
+  flex: 0 1  auto;
+  justify-content: center;
+  padding: 8px;
+
+  &:hover {
+    color: ${colors.blackColor};
+    border-color: ${colors.blackColor};
+  }
+`;
+
+const DropdownContainer = SC.div`
+  background-color: ${colors.primaryColor};
+  border-bottom: 1px solid ${colors.primaryBorderColor};
+`;
+
+const DropdownLink = SC.div`
+  color: ${colors.whiteColor};
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: 1.5px;
+  padding: 16px;
+
+  &:hover {
+    background-color: ${colors.whiteColor};
+    color: ${colors.primaryColor};
+  }
 `;
 
 export default function Header() {
-  return <HeaderContainer>Header</HeaderContainer>;
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
+  const DropdownComponents = () => (
+    <DropdownContainer>
+      <DropdownLink>Link1</DropdownLink>
+      <DropdownLink>Link1</DropdownLink>
+      <DropdownLink>Link1</DropdownLink>
+      <DropdownLink>Link1</DropdownLink>
+    </DropdownContainer>
+  );
+
+  return (
+    <div>
+      <HeaderContainer>
+        <TitleBar>Willy Tan</TitleBar>
+        <HamburgerButton
+          onClick={() => setDropdownVisibility(!dropdownVisibility)}
+        >
+          <FaBars />
+        </HamburgerButton>
+      </HeaderContainer>
+      {dropdownVisibility && <DropdownComponents />}
+    </div>
+  );
 }
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primaryColor: "#eb3434",
+  primaryBorderColor: "#bf2222",
+  whiteColor: "#ffffff",
+  blackColor: "black"
+};
