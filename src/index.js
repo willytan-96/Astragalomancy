237e4bcd26074f3f38c8341c735a66c56e9839ba
Willy Tan <willy.tan434@gmail.com>
2019-08-08 22:59:33 +0700

    Add dropdown view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./router";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router />
     </div>
   );
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import Header from "../../components/Header";
 
 export default () => {
-  return (
-    <Fragment>
-      <Header />
-    </Fragment>
-  );
+  return <Fragment>bodyWelcome</Fragment>;
 };
